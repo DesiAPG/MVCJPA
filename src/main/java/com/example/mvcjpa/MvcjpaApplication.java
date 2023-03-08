5115543a011f7d7b60f0698298062c528b0b70a7
DesiAPG <74121435+DesiAPG@users.noreply.github.com>
2023-03-08 10:53:54 -0500

    First commit

---
+++ src/main/java/com/example/mvcjpa/MvcjpaApplication.java
@@ -0,0 +1,13 @@
+package com.example.mvcjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcjpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MvcjpaApplication.class, args);
+    }
+
+}
