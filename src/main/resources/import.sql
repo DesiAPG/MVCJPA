945dcd63b3ec661c677295ea849afc9c9e574c1f
DesiAPG <74121435+DesiAPG@users.noreply.github.com>
2023-03-09 20:15:25 -0500

    Create import.sql

---
+++ src/main/resources/import.sql
@@ -0,0 +1,4 @@
+INSERT INTO Clients (id, name, surname, email, create_at)
+VALUES (1, 'Duvan', 'Marin', 'duvan@mail.com', '2022-03-08');
+INSERT INTO Clients (id, name, surname, email, create_at)
+VALUES (2, 'Camilo', 'Camacho', 'camilo@mail.com', '2022-03-08');
