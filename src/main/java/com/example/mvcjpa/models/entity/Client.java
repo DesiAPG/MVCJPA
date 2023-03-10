d3436de963e58583e7fc4f5f7a8cbb000513ccae
DesiAPG <74121435+DesiAPG@users.noreply.github.com>
2023-03-09 20:15:23 -0500

    Create Client.java

---
+++ src/main/java/com/example/mvcjpa/models/entity/Client.java
@@ -0,0 +1,63 @@
+package com.example.mvcjpa.models.entity;
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+import java.util.Date;
+
+@Entity
+@Table(name = "clients")
+public class Client implements Serializable {
+
+    private static final long serialVersionUID = 2L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "client_name")
+    private String name;
+    private String surname;
+    private String email;
+    @Column(name = "create_at")
+    @Temporal(TemporalType.DATE)
+    private Date createAt;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getCreateAt() {
+        return createAt;
+    }
+
+    public void setCreateAt(Date createAt) {
+        this.createAt = createAt;
+    }
+}
